4371ab42efd6c7c262689d1686fec40099513b1a
Ryael <Rayrah@live.com>
2022-10-18 21:32:25 +0000

    Add script to display leaderboards section and hide the main menu upon click for the leaderboards button

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,11 @@ function startGameButton() {
     document.getElementById("game-area").style.display = "flex";
     document.getElementById("rules-difficulty").style.display = "block";
 }
+
+/** Leaderboards
+ */
+ function leaderboardsButton() {
+    document.getElementById("main-menu").style.display = "none";
+    document.getElementById("game-area").style.display = "flex";
+    document.getElementById("leaderboards").style.display = "block";
+}
