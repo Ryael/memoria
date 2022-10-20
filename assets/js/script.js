8a2755a1685062d170fef4d977b5d61e5320ce70
Ryael <Rayrah@live.com>
2022-10-20 20:36:42 +0000

    Clean and re-organise scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,81 @@
 /* Button Scripts */
 
 /** Hides Section by ID */
-function hideSection(id) {
-    document.getElementById(id).style.display = "none";
+function hideSection(item) {
+    item.style.display = "none";
 }
 
 /** Shows Section by ID as Flex */
-function showSectionFlex(id) {
-    document.getElementById(id).style.display = "flex";
+function showSectionFlex(item) {
+    item.style.display = "flex";
 }
 
 /** Shows Section by ID as Block */
-function showSectionBlock(id) {
-    document.getElementById(id).style.display = "block";
+function showSectionBlock(item) {
+    item.style.display = "block";
+}
+
+/** Sets constants for menu navigation */
+const idSelectors = {
+    mainMenu: document.querySelector("#main-menu"),
+    gameArea: document.querySelector("#game-area"),
+    rulesDifficulty: document.querySelector("#rules-difficulty"),
+    leaderboards: document.querySelector("#leaderboards"),
+    credits: document.querySelector("#credits"),
+    backButton: document.querySelector("#back-button"),
+    memoryGame: document.querySelector("#memory-game")
 }
 
 /** Start Game */
 function startGameButton() {
-    hideSection("main-menu");
-    showSectionFlex("game-area");
-    showSectionBlock("rules-difficulty");
-    showSectionFlex("back-button");
+    hideSection(idSelectors.mainMenu);
+    showSectionFlex(idSelectors.gameArea);
+    showSectionBlock(idSelectors.rulesDifficulty);
+    showSectionFlex(idSelectors.backButton);
 }
 
 /** Easy Mode */
 function easyGameButton() {
-    hideSection("rules-difficulty");
-    showSectionFlex("game-area");
-    showSectionFlex("memory-game");
+    hideSection(idSelectors.rulesDifficulty);
+    showSectionFlex(idSelectors.gameArea);
+    showSectionFlex(idSelectors.memoryGame);
 }
 
 /** Leaderboards */
  function leaderboardsButton() {
-    hideSection("main-menu");
-    showSectionFlex("game-area");
-    showSectionBlock("leaderboards");
-    showSectionFlex("back-button");
+    hideSection(idSelectors.mainMenu);
+    showSectionFlex(idSelectors.gameArea);
+    showSectionBlock(idSelectors.leaderboards);
+    showSectionFlex(idSelectors.backButton);
 }
 
 /** Credits */
  function creditsButton() {
-    hideSection("main-menu");
-    showSectionFlex("game-area");
-    showSectionBlock("credits");
-    showSectionFlex("back-button");
+    hideSection(idSelectors.mainMenu);
+    showSectionFlex(idSelectors.gameArea);
+    showSectionBlock(idSelectors.credits);
+    showSectionFlex(idSelectors.backButton);
 }
 
 /** Return to Main Menu */
-function backButton() {
-    showSectionFlex("main-menu");
-    hideSection("game-area");
-    hideSection("credits");
-    hideSection("rules-difficulty");
-    hideSection("leaderboards");
-    hideSection("back-button");
-    hideSection("memory-game");
+function returnButton() {
+    showSectionFlex(idSelectors.mainMenu);
+    hideSection(idSelectors.gameArea);
+    hideSection(idSelectors.credits);
+    hideSection(idSelectors.rulesDifficulty);
+    hideSection(idSelectors.leaderboards);
+    hideSection(idSelectors.backButton);
+    hideSection(idSelectors.memoryGame);
 }
 
+/* Memory Game */
+
+/** Defines the selectors used throughout the game */
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.board'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('button'),
+    win: document.querySelector('.win')
+}
