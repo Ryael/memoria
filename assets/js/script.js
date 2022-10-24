0165a5e71f7156348e6daba8e9f74bba85ac67c7
Ryael <Rayrah@live.com>
2022-10-24 04:03:52 +0000

    Add confetti script that plays on win

--- assets/js/script.js
+++ assets/js/script.js
@@ -234,6 +234,7 @@ const flipCard = card => {
             `
 
             clearInterval(state.loop) // Stops the game loop.
+            displayConfetti() // Displays confetti.
         }, 1000)
     }
 }
@@ -246,6 +247,33 @@ const flipBackCards = () => {
     })
 
     state.flippedCards = 0 // Reset flippedCards state to 0.
+}
+
+/* Confetti
+Source: https://www.kirilv.com/canvas-confetti/ */
+
+/** Displays Confetti */
+function displayConfetti() {
+    let duration = 10 * 1000;
+    let animationEnd = Date.now() + duration;
+    let defaults = { startVelocity: 30, spread: 360, ticks: 30, zIndex: 0 };
+
+    function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+    }
+
+    let interval = setInterval(function() {
+    let timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+        return clearInterval(interval);
+    }
+
+    let particleCount = 50 * (timeLeft / duration);
+    // since particles fall down, start a bit higher than random
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 500);
 }
 
 /* Calling Functions */
