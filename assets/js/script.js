6a1f6904a9af3e1e4eeb30c75cbbb750bbab199b
Ryael <Rayrah@live.com>
2022-10-26 15:37:31 +0000

    Add confetti script back after it was accidentally removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -291,3 +291,30 @@ const flipBackCards = () => {
     state.flippedCards = 0; // Reset flippedCards state to 0.
     state.disableFlip = false;
 }
+
+/* Confetti
+Source: https://www.kirilv.com/canvas-confetti/ */
+
+/** Displays Confetti */
+function displayConfetti() {
+    let duration = 10 * 1000; // Duration of the confetti.
+    let animationEnd = Date.now() + duration; // End of the confetti.
+    let defaults = { startVelocity: 30, spread: 360, ticks: 30, zIndex: 0 }; // Spread, amount, and intensity of the initial confetti blast.
+
+    function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+    }
+
+    let interval = setInterval(function() {
+    let timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+        return clearInterval(interval);
+    }
+
+    let particleCount = 50 * (timeLeft / duration);
+    // Since particles fall down, start a bit higher than random. Also the speed of the confetti.
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+    confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+    }, 500);
+}
